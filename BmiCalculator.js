fcac942bed369ff391731287589bf08ebd7957d2
mnubhan <76425959+mnubhan@users.noreply.github.com>
2022-08-07 22:55:43 +0800

    Update BmiCalculator.js

--- BmiCalculator.js
+++ BmiCalculator.js
@@ -6,3 +6,16 @@ const bmiCalculator = (weight,height) => {
 } 
 
 bmiCalculator(65,180)
+
+function bmiCalculator (weight, height) {
+    let bmi = Math.round(weight/Math.pow(height,2))
+    if(bmi < 18.5){
+        return "Your BMI is " + bmi + " so you are underweight."
+    } 
+    if(bmi>=18.5 && bmi <= 24.9 ) {
+        return "Your BMI is " + bmi + " , so you have a normal weight."
+    }
+    else{
+        return "Your BMI is " +bmi + " so you are overweight."
+    }
+}
